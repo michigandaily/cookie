57436a8095280e49712e0e3a185bf3f57d4ae394
erxclau <ericlau41802@gmail.com>
2022-10-13 06:07:14 -0400

    Add white background to client side download

--- src/graphic/js/download-image.js
+++ src/graphic/js/download-image.js
@@ -2,7 +2,7 @@ const downloadImage = async (message) => {
   const { format, width } = JSON.parse(message);
   const { toSvg, toPng } = await import("html-to-image");
   const converter = format === "svg" ? toSvg : toPng;
-  const imgurl = await converter(document.body);
+  const imgurl = await converter(document.body, { backgroundColor: "white" });
   const a = document.createElement("a");
   a.href = imgurl;
   a.download = `cookie-graphic-${new Date().toISOString()}-${width}.${format}`;
