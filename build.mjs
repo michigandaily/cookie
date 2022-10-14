e9cd12605e9aff03056f94c96c9bb2f2b1aa4434
erxclau <ericlau41802@gmail.com>
2022-10-13 22:13:49 -0400

    Fail gracefully with missing browser

--- build.mjs
+++ build.mjs
@@ -36,44 +36,62 @@ const main = async () => {
     ],
   });
 
-  const subscriber = await bundler.watch();
+  const subscriber = await bundler.watch(async (err, event) => {
+    if (err) {
+      throw err;
+    }
 
-  const browser = await chromium.launch({
-    channel: "chrome",
+    if (event.type === "buildSuccess") {
+      let browser;
+      try {
+        browser = await chromium.launch({
+          channel: "chrome",
+        });
+      } catch {
+        console.error(
+          "Could not find Chrome binary. Skipping screenshot script."
+        );
+        await subscriber.unsubscribe();
+        return;
+      }
+
+      const page = await browser.newPage();
+      const screenSizes = [780, 338, 288];
+
+      const downloadAndSave = async (entry, size, format) => {
+        await page.locator("#graphic > iframe").waitFor("attached");
+        await page
+          .mainFrame()
+          .childFrames()
+          .pop()
+          .waitForLoadState("networkidle");
+
+        const [download] = await Promise.all([
+          page.waitForEvent("download"),
+          await page.locator(`button#download-${format}`).click(),
+        ]);
+
+        const entryName = basename(entry, ".html");
+        const path = `./img/cookie-graphic-${entryName}-${size}-${new Date().toISOString()}.${format}`;
+        console.log("Saving screenshot to", path);
+        await download.saveAs(path);
+      };
+
+      const shots = entries
+        .map((entry) => screenSizes.map((size) => ({ entry, size })))
+        .flat();
+
+      for await (const { entry, size } of shots) {
+        await page.goto(`${baseUrl}?width=${size}&entry=${entry}`, {
+          waitUntil: "networkidle",
+        });
+        await downloadAndSave(entry, size, "png");
+        await downloadAndSave(entry, size, "svg");
+      }
+      await browser.close();
+      await subscriber.unsubscribe();
+    }
   });
-
-  const page = await browser.newPage();
-  const screenSizes = [780, 338, 288];
-
-  const downloadAndSave = async (entry, size, format) => {
-    await page.locator("#graphic > iframe").waitFor("attached");
-    await page.mainFrame().childFrames().pop().waitForLoadState("networkidle");
-
-    const [download] = await Promise.all([
-      page.waitForEvent("download"),
-      await page.locator(`button#download-${format}`).click(),
-    ]);
-
-    const entryName = basename(entry, ".html");
-    const path = `./img/cookie-graphic-${entryName}-${size}-${new Date().toISOString()}.${format}`;
-    console.log("Saving screenshot to", path);
-    await download.saveAs(path);
-  };
-
-  const shots = entries
-    .map((entry) => screenSizes.map((size) => ({ entry, size })))
-    .flat();
-
-  for await (const { entry, size } of shots) {
-    await page.goto(`${baseUrl}?width=${size}&entry=${entry}`, {
-      waitUntil: "networkidle",
-    });
-    await downloadAndSave(entry, size, "png");
-    await downloadAndSave(entry, size, "svg");
-  }
-
-  await browser.close();
-  await subscriber.unsubscribe();
 };
 
 main();
