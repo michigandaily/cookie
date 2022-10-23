da06e5776c35342b7867449b30f4587cf4b5a1b0
erxclau <ericlau41802@gmail.com>
2022-10-22 21:48:52 -0400

    Add homepage option

--- src/graphic/js/display-options.js
+++ src/graphic/js/display-options.js
@@ -15,6 +15,10 @@ const setDisplayOptions = () => {
   if (shouldHide("note")) hide(".figure__note");
   if (shouldHide("source")) hide(".figure__source");
   if (shouldHide("byline")) hide(".figure__byline");
+
+  if (q.get("homepage") === "true") {
+    document.body.classList.add("homepage");
+  }
 };
 
 export default setDisplayOptions;
