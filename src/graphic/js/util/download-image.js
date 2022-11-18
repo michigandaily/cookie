ffe0f89d1a0a6e5c109ceac40c188a6be3894486
Eric Lau <44459660+erxclau@users.noreply.github.com>
2022-11-18 04:06:15 -0500

    Use dom-to-svg over html-to-image for svg export (#71)
    * Use dom-to-svg over html-to-image for svg export
    
    * Remove `inlineResources`
    
    Inlining resources dramatically increases the size of the SVG,
    particularly when inlining fonts. Just defer to Illustrator to have the
    font.

--- src/graphic/js/util/download-image.js
+++ src/graphic/js/util/download-image.js
@@ -1,13 +1,20 @@
 const downloadImage = async (message) => {
   const { entry, format, width } = JSON.parse(message);
-  const { toSvg, toPng } = await import("html-to-image");
-  const converter = format === "svg" ? toSvg : toPng;
-  const imgurl = await converter(document.body, {
-    width: width,
-    backgroundColor: "white",
-  });
   const a = document.createElement("a");
-  a.href = imgurl;
+  if (format === "svg") {
+    const { elementToSVG } = await import("dom-to-svg");
+    const xml = elementToSVG(document.body);
+    const svgString = new XMLSerializer().serializeToString(xml);
+    a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(svgString)}`;
+  } else if (format === "png") {
+    const { toPng } = await import("html-to-image");
+    const imgurl = await toPng(document.body, {
+      width: width,
+      backgroundColor: "white",
+    });
+    a.href = imgurl;
+  }
+
   a.download = `cookie-graphic-${entry}-${width}-${new Date().toISOString()}.${format}`;
   a.click();
   a.remove();
