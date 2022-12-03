b26c0433766a83e71a383340bebc4f7be04106f0
Eric Lau <44459660+erxclau@users.noreply.github.com>
2022-12-03 04:19:29 -0500

    Remove dependency for PNG download (#75)

--- src/graphic/js/util/download-image.js
+++ src/graphic/js/util/download-image.js
@@ -1,23 +1,38 @@
 const downloadImage = async (message) => {
   const { entry, format, width } = JSON.parse(message);
   const a = document.createElement("a");
-  if (format === "svg") {
-    const { elementToSVG } = await import("dom-to-svg");
-    const xml = elementToSVG(document.body);
-    const svgString = new XMLSerializer().serializeToString(xml);
-    a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(svgString)}`;
-  } else if (format === "png") {
-    const { toPng } = await import("html-to-image");
-    const imgurl = await toPng(document.body, {
-      width: width,
-      backgroundColor: "white",
-    });
-    a.href = imgurl;
-  }
+  const { elementToSVG } = await import("dom-to-svg");
+  const xml = elementToSVG(document.body);
+  const svgString = new XMLSerializer().serializeToString(xml);
 
   a.download = `cookie-graphic-${entry}-${width}-${new Date().toISOString()}.${format}`;
-  a.click();
-  a.remove();
+  if (format === "svg") {
+    a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(svgString)}`;
+    a.click();
+    a.remove();
+  } else if (format === "png") {
+    const img = new Image();
+
+    img.onload = () => {
+      const dpr = window.devicePixelRatio ?? 1;
+
+      const canvas = document.createElement("canvas");
+      canvas.width = width * dpr;
+      canvas.height = document.body.clientHeight * dpr;
+
+      const context = canvas.getContext("2d");
+      context.scale(dpr, dpr);
+      context.fillStyle = "white";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(img, 0, 0);
+
+      a.href = canvas.toDataURL("image/png");
+      a.click();
+      a.remove();
+    };
+
+    img.src = `data:image/svg+xml;utf8,${svgString}`;
+  }
 };
 
 export default downloadImage;
