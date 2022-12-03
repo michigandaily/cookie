7e5fae7ee34783a23fd15af12b9a37ed1cfe0f9d
Eric Lau <44459660+erxclau@users.noreply.github.com>
2022-12-03 03:39:03 -0500

    Use Vite (#74)
    * Remove whitespace from Nunjucks templating
    
    https://mozilla.github.io/nunjucks/templating.html#whitespace-control
    
    * Remove more nunjucks whitespace
    
    * Remove minify SVG configuration
    
    * Rewrite build script
    
    * Add additonal vite configuration

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { resolve, parse } from "node:path";
+import { defineConfig } from "vite";
+import nunjucks from "@michigandaily/vite-plugin-transform-nunjucks";
+import dsv from "@michigandaily/rollup-plugin-dsv";
+
+import { entries } from "./config.json";
+const graphics = Object.assign(
+  ...Object.keys(entries)
+    .map((entry) => parse(entry))
+    .map(({ name, base }) => ({
+      [name]: resolve(__dirname, `src/graphic/${base}`),
+    }))
+);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [nunjucks(), dsv({ include: ["**.csv", "**.tsv", "**.dsv"] })],
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        ...graphics,
+      },
+    },
+  },
+});
