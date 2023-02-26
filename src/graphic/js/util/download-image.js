a70016b8e3e572bd51483c838ff0ec2367e40df0
Eric Lau <44459660+erxclau@users.noreply.github.com>
2023-02-26 02:15:12 -0500

    Fix fonts in PNG screenshot (#84)

--- src/graphic/js/util/download-image.js
+++ src/graphic/js/util/download-image.js
@@ -1,38 +1,26 @@
 const downloadImage = async (message) => {
   const { entry, format, width } = JSON.parse(message);
   const a = document.createElement("a");
-  const { elementToSVG } = await import("dom-to-svg");
-  const xml = elementToSVG(document.body);
-  const svgString = new XMLSerializer().serializeToString(xml);
-
   a.download = `cookie-graphic-${entry}-${width}-${new Date().toISOString()}.${format}`;
-  if (format === "svg") {
+
+  if (format === "png") {
+    const { toPng } = await import("html-to-image");
+    const url = await toPng(document.body, {
+      backgroundColor: "white",
+      width,
+    });
+    a.href = url;
+  } else if (format === "svg") {
+    const { elementToSVG } = await import("dom-to-svg");
+    const xml = elementToSVG(document.body.querySelector("svg"));
+    const svgString = new XMLSerializer().serializeToString(xml);
     a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(svgString)}`;
-    a.click();
-    a.remove();
-  } else if (format === "png") {
-    const img = new Image();
-
-    img.onload = () => {
-      const dpr = window.devicePixelRatio ?? 1;
-
-      const canvas = document.createElement("canvas");
-      canvas.width = width * dpr;
-      canvas.height = document.body.clientHeight * dpr;
-
-      const context = canvas.getContext("2d");
-      context.scale(dpr, dpr);
-      context.fillStyle = "white";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(img, 0, 0);
-
-      a.href = canvas.toDataURL("image/png");
-      a.click();
-      a.remove();
-    };
-
-    img.src = `data:image/svg+xml;base64,${window.btoa(svgString)}`;
+  } else {
+    throw new Error(`Unknown format: ${format}`);
   }
+
+  a.click();
+  a.remove();
 };
 
 export default downloadImage;
