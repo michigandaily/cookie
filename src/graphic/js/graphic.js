add7323a2173aa8536497ab2c0ae82fe4d3e471d
erxclau <ericlau41802@gmail.com>
2023-02-26 01:19:50 -0500

    Add bore as default dependency

--- src/graphic/js/graphic.js
+++ src/graphic/js/graphic.js
@@ -1,16 +1,28 @@
 // JS for your graphic
 import pym from "pym.js";
-// import * as d3 from "d3";
+import { select } from "d3";
+import { build, BarChart } from "@michigandaily/bore";
 
 import downloadImage from "./util/download-image";
 import setDisplayOptions from "./util/set-display";
 
 const draw = () => {
   // step 1: access data
+  const data = new Map().set("A", 5).set("B", 10).set("C", 2).set("D", 6);
+
   // step 2: create chart dimensions
+  const height = 150;
+  const margin = { left: 0, right: 20, bottom: 20, top: 20 };
+
   // step 3: draw canvas
+  const svg = select("figure").append("svg");
+
   // step 4: create scales
+
   // step 5: draw data
+  const chart = new BarChart().height(height).margin(margin).wrappx(0);
+  svg.datum(data).call(build(chart));
+
   // step 6: draw peripherals
   // step 7: set up interactions
 };
